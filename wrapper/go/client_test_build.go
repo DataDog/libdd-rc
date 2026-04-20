aa2295a6e6a1877f0e017df218b46938e2bad63a
Dom Dwyer <dom@itsallbroken.com>
2026-04-20 12:42:44 +0200

    refactor: test-harness -> test-harness-ffi
    We will have more test harness crates for the other crates.

--- wrapper/go/client_test_build.go
+++ wrapper/go/client_test_build.go
@@ -15,7 +15,7 @@
 package libddrc
 
 /*
-#cgo LDFLAGS: -L../../target/debug -lrc_x509_test_harness -Wl,-rpath,${SRCDIR}/../../target/debug
+#cgo LDFLAGS: -L../../target/debug -lrc_x509_test_harness_ffi -Wl,-rpath,${SRCDIR}/../../target/debug
 #include <stdlib.h>
 #include "libdd_rc_test_harness.h"
 
