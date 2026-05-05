6025bc909bc528f49ec117b2ee9a4d77ac560e95
Dom Dwyer <dom@itsallbroken.com>
2026-05-05 15:14:05 +0200

    feat(proto): define Magic Tunnel protocol
    This change extends the core protocol with the types to support the
    Magic Tunnel RPC call functionality.
    
    For the core Magic Tunnel protocol, this adds:
    
      * High level Magic tunnel request / response protocol types.
    
      * Namespacing of magic tunnel RPCs. This is referred to as a "Topic"
        in the design doc[1], but I changed it to "Namespace" when
        implementing it to try and communicate the intent better - everyone
        is familiar with namespacing and why it's used, and therefore hints
        as to how it should be used (though it's documented too).
    
      * Dispatch error reporting.
    
    In addition an example integration is added (RemoteConfigRequest to
    obtain an Agent flare) that outlines how to structure the RPC types for
    backwards compatibility / future extensibility.
    
    [1]: https://docs.google.com/document/d/1PtgvesgC0hmIUbGyAUpcwUcUwLiNSZU845B9okY2R9o/edit?usp=sharing

--- lib/rc-x509-proto/build.rs
+++ lib/rc-x509-proto/build.rs
@@ -14,23 +14,45 @@
 
 //! Protobuf compilation / codegen.
 
-fn main() -> std::io::Result<()> {
-    println!("cargo::rerun-if-changed=protos/protocol.proto");
+use glob::glob;
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
 
     // A list of paths to fields that use `Bytes`, which need a manual impl of
     // Arbitrary defined to avoid compilation errors caused by derive(Arbitrary)
     // not being implemented for Bytes.
-    let bytes_fields = ["rc.x509.protocol.v1.Certificate.der"];
+    let bytes_fields = [
+        "rc.x509.protocol.v1.Certificate.der",
+        "rc.x509.magic_tunnel.v1.MagicTunnelRequest.payload",
+    ];
     for v in bytes_fields {
         config.field_attribute(v, r#"#[proptest(strategy = "crate::arbitrary_bytes()")]"#);
+    }
+
+    // The `response` field is inside a `oneof`, so it becomes an enum variant
+    // (`Result::Response(Bytes)`) rather than a struct field. The proptest
+    // `strategy` attribute on an enum variant must produce the full enum value,
+    // not just the inner field, so we use a dedicated helper.
+    config.field_attribute(
+        "rc.x509.magic_tunnel.v1.MagicTunnelResponse.result.response",
+        r#"#[proptest(strategy = "crate::arbitrary_oneof_bytes(Self::Response)")]"#,
+    );
+
+    // Discover all the protobuf files.
+    let mut protos = vec![];
+    for entry in glob("protos/**/*.proto").expect("invalid glob") {
+        let v = entry?;
+        let v = v.to_str().expect("valid unicode path");
+
+        println!("cargo::rerun-if-changed={v}");
+        protos.push(v.to_owned());
     }
 
     config
         .bytes(["."])
         .type_attribute(".", "#[derive(proptest_derive::Arbitrary)]")
-        .compile_protos(&["protos/protocol.proto"], &["protos/"])?;
+        .compile_protos(&protos, &["protos"])?;
 
     Ok(())
 }
---
+++ lib/rc-x509-proto/protos/magic_tunnel/README.md
@@ -0,0 +1,7 @@
+# Magic Tunnel Protocol
+
+Check out the [`protocol.proto`] file for the core protocol types and
+implementation suggestions.
+
+THe [`remote_config.proto`] file shows an example integration, making use of
+subtopics to enable future extensibility.
