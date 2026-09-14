cfd3117abeaa46774e78de98ad1d10e306d9912d
Kyle Ames <kyle.ames@datadoghq.com>
2026-09-14 14:16:16 -0400

    Add a log sink for tracing events to the FFI for non-Rust clients
    Exposes a way for non-Rust native clients to provide a file descriptor
    where tracing events will be emitted to allow collection of
    rc-x509-client internal logs.

--- ffi-hosts/go/cmd/testx509/main.go
+++ ffi-hosts/go/cmd/testx509/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/DataDog/libdd-rc/ffi-hosts/go/rcx509"
 )
@@ -12,6 +13,12 @@ import (
 const debugServicePingURI = "rc.x509.magic_tunnel.remote_config.v1.DebugService/Ping"
 
 func main() {
+	// Surface the client library's own tracing output on stderr, interleaved
+	// with this example's own logging, for local debugging.
+	if err := rcx509.EnableLogSink(rcx509.LogLevelDebug, os.Stderr); err != nil {
+		log.Fatal(err)
+	}
+
 	client, err := rcx509.NewClient("wss://config.datad0g.com/api/v2/ws")
 	if err != nil {
 		log.Fatal(err)
